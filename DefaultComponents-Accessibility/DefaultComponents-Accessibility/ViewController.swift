f5bfc795f0d94a47693ebdbc43aa0d5c0bf54827
링키지랩 <linkagelab@lingkijilaeb-ui-MacBookPro.local>
2024-09-09 12:03:15 +0900

    Create a project DefaultComponents-Accssibility

---
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DefaultComponents-Accessibility
+//
+//  Created by 링키지랩 on 9/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
