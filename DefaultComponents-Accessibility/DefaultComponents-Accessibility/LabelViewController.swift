af9cd1002579a4c8b5e6d492d3932b978df1cd40
링키지랩 <linkagelab@lingkijilaeb-ui-MacBookPro.local>
2024-09-09 15:14:00 +0900

    [FEAT] Label: UI 구성 #3

---
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/LabelViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LabelViewController.swift
+//  DefaultComponents-Accessibility
+//
+//  Created by 링키지랩 on 9/9/24.
+//
+
+import UIKit
+
+final class LabelViewController: UIViewController {
+    
+    lazy var label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSubViews()
+        configureView()
+        configureConstraints()
+    }
+}
+
+private extension LabelViewController {
+    func configureSubViews() {
+        label.text = "링키지랩"
+        label.font = .systemFont(ofSize: 30)
+        label.textColor = .black
+    }
+    
+    func configureView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    func configureConstraints() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        
+        let verticalInset: CGFloat = 20
+        let horizontalInset: CGFloat = 10
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: verticalInset),
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: horizontalInset)
+        ])
+    }
+}
