d22857a16c73124165e725b6364ceb44002ffa79
88yhtserof <88yhtserofg@gmail.com>
2024-09-10 12:30:47 +0900

    [FEAT] Text: TextField 구현 #7

--- DefaultComponents-Accessibility/DefaultComponents-Accessibility/TextViewController/TextViewController.swift
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/TextViewController/TextViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class TextViewController: UIViewController {
     
     lazy var label = UILabel()
+    lazy var textField = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,12 @@ private extension TextViewController {
         label.text = "링키지랩"
         label.font = .systemFont(ofSize: 30)
         label.textColor = .black
+        
+        textField.font = .systemFont(ofSize: 30)
+        textField.placeholder = "원하는 텍스트를 입력하십시오"
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.borderWidth = CGFloat(2.0)
+        textField.clearButtonMode = .whileEditing
     }
     
     func configureView() {
@@ -31,15 +38,22 @@ private extension TextViewController {
     }
     
     func configureConstraints() {
-        label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
+        [ label, textField ]
+            .forEach{
+                $0.translatesAutoresizingMaskIntoConstraints = false
+                view.addSubview($0)
+            }
         
         let verticalInset: CGFloat = 20
         let horizontalInset: CGFloat = 10
         
         NSLayoutConstraint.activate([
             label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: verticalInset),
-            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset)
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
+            
+            textField.topAnchor.constraint(equalTo: label.bottomAnchor, constant: verticalInset),
+            textField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
+            textField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -horizontalInset)
         ])
     }
 }
