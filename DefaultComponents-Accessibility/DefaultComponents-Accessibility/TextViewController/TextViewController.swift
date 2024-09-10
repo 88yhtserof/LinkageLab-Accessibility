34269ead43cdc4039ba8955731bcc51a2abccc10
88yhtserof <88yhtserofg@gmail.com>
2024-09-10 12:38:27 +0900

    [FEAT] Text: textView 구현 #7

--- DefaultComponents-Accessibility/DefaultComponents-Accessibility/TextViewController/TextViewController.swift
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/TextViewController/TextViewController.swift
@@ -11,6 +11,7 @@ final class TextViewController: UIViewController {
     
     lazy var label = UILabel()
     lazy var textField = UITextField()
+    lazy var textView = UITextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +32,9 @@ private extension TextViewController {
         textField.layer.borderColor = UIColor.black.cgColor
         textField.layer.borderWidth = CGFloat(2.0)
         textField.clearButtonMode = .whileEditing
+        
+        textView.font = .systemFont(ofSize: 20)
+        textView.backgroundColor = .systemGray6
     }
     
     func configureView() {
@@ -38,7 +42,7 @@ private extension TextViewController {
     }
     
     func configureConstraints() {
-        [ label, textField ]
+        [ label, textField, textView ]
             .forEach{
                 $0.translatesAutoresizingMaskIntoConstraints = false
                 view.addSubview($0)
@@ -53,7 +57,12 @@ private extension TextViewController {
             
             textField.topAnchor.constraint(equalTo: label.bottomAnchor, constant: verticalInset),
             textField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
-            textField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -horizontalInset)
+            textField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -horizontalInset),
+            
+            textView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: verticalInset),
+            textView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
+            textView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -horizontalInset),
+            textView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -verticalInset)
         ])
     }
 }
