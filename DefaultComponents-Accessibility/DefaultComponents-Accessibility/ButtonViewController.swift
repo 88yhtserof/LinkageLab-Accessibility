325c775b0f0e21e3f7d45a89e74cdb22d071834f
88yhtserof <88yhtserofg@gmail.com>
2024-09-10 11:09:50 +0900

    [FEAT] Image Button 구현  #5

--- DefaultComponents-Accessibility/DefaultComponents-Accessibility/ButtonViewController.swift
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/ButtonViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 final class ButtonViewController: UIViewController {
     
     lazy var textButton = UIButton()
+    lazy var imageButton = UIButton()
+    
+    private var imageButtonConfiguration = UIButton.Configuration.plain()
+    private var isLightOn: Bool = false {
+        didSet {
+            imageButtonConfiguration.image = isLightOn ? imageLightOff : imageLightOn
+            imageButton.configuration = imageButtonConfiguration
+        }
+    }
+    private var imageLightOn = UIImage(named: "light_on")
+    private var imageLightOff = UIImage(named: "light_off")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +32,14 @@ final class ButtonViewController: UIViewController {
 
 private extension ButtonViewController {
     func configureSubViews() {
-        var configuration = UIButton.Configuration.plain()
-        configuration.title = "텍스트 타입"
-        textButton.configuration = configuration
+        var textConfiguration = UIButton.Configuration.plain()
+        textConfiguration.title = "텍스트 타입"
+        textButton.configuration = textConfiguration
+        
+        imageButtonConfiguration.image = imageLightOff
+        imageButton.configuration = imageButtonConfiguration
+        imageButton.clipsToBounds = true
+        imageButton.addTarget(self, action: #selector(didTouchUpInside), for: .touchUpInside)
     }
     
     func configureView() {
@@ -31,8 +47,11 @@ private extension ButtonViewController {
     }
     
     func configureConstraints() {
-        textButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(textButton)
+        [ textButton, imageButton ]
+            .forEach{
+                $0.translatesAutoresizingMaskIntoConstraints = false
+                view.addSubview($0)
+            }
         
         let verticalInset: CGFloat = 20
         let horizontalInset: CGFloat = 10
@@ -40,7 +59,17 @@ private extension ButtonViewController {
         NSLayoutConstraint.activate([
             textButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: verticalInset),
             textButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
-            textButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: horizontalInset)
+            textButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: horizontalInset),
+            
+            imageButton.topAnchor.constraint(equalTo: textButton.bottomAnchor, constant: verticalInset),
+            imageButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
+            imageButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: horizontalInset),
         ])
+    }
+}
+
+extension ButtonViewController {
+    @objc func didTouchUpInside(_ sender: UIButton) {
+        isLightOn.toggle()
     }
 }
