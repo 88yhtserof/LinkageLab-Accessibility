e62e5e2eec3b3776b488a41c02a2af4a4d505650
88yhtserof <88yhtserofg@gmail.com>
2024-09-13 13:53:24 +0900

    [FEAT] Alert: 기본 버튼 배치 #15

---
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/AlertViewController.swift
@@ -0,0 +1,75 @@
+//
+//  AlertViewController.swift
+//  DefaultComponents-Accessibility
+//
+//  Created by 링키지랩 on 9/13/24.
+//
+
+import UIKit
+
+final class AlertViewController: UIViewController {
+    
+    lazy var stackView = UIStackView()
+    lazy var alertForMessageButton = UIButton()
+    lazy var alertForSelectButton = UIButton()
+    lazy var alertForViewButton = UIButton()
+    lazy var actionSheetForActionButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSubViews()
+        configureView()
+        configureConstraints()
+    }
+}
+
+// MARK: Configuration
+private extension AlertViewController{
+    func configureSubViews() {
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        
+        var configuration = UIButton.Configuration.filled()
+        configuration.baseBackgroundColor = .blue
+        
+        let descriptions = [
+            "Aler: 안내",
+            "Alert: 선택",
+            "Alert: View",
+            "ActionSheet: 동작"
+        ]
+        let buttons = [ alertForMessageButton, alertForSelectButton, alertForViewButton, actionSheetForActionButton ]
+        
+        zip(buttons, descriptions)
+            .forEach{ (button, description) in
+                configuration.title = description
+                button.configuration = configuration
+            }
+    }
+    
+    func configureView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    func configureConstraints() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        [ alertForMessageButton, alertForSelectButton, alertForViewButton, actionSheetForActionButton ]
+            .forEach{
+                $0.translatesAutoresizingMaskIntoConstraints = false
+                stackView.addArrangedSubview($0)
+            }
+        
+        let verticalInset: CGFloat = 20
+        let horizontalInset: CGFloat = 50
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: horizontalInset),
+            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -horizontalInset)
+        ])
+    }
+}
