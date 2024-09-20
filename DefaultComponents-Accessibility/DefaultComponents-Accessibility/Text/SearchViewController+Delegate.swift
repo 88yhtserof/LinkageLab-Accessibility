97b013cf3eadd0862d0e5042f0ba6fc2212960df
88yhtserof <88yhtserofg@gmail.com>
2024-09-20 09:46:34 +0900

    [FEAT] Search: 화면 구성 및 검색 기능 구현  #19

---
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/Text/SearchViewController+Delegate.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController+Delegate.swift
+//  DefaultComponents-Accessibility
+//
+//  Created by 링키지랩 on 9/19/24.
+//
+
+import UIKit
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        if let searchWord = searchBar.text {
+            filteredSnapshot(searchWord: searchWord)
+        }
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        initialSnapshot()
+    }
+    
+}
---
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/Text/SearchViewController.swift
@@ -0,0 +1,80 @@
+//
+//  SearchViewController.swift
+//  DefaultComponents-Accessibility
+//
+//  Created by 링키지랩 on 9/19/24.
+//
+
+import UIKit
+
+final class SearchViewController: UIViewController {
+    
+    var dataSource: DataSource!
+    var snapshot: Snapshot!
+    
+    var navigationTitle: String? {
+        didSet {
+            navigationItem.title = navigationTitle
+        }
+    }
+    
+    let samples = Outline.sections
+    
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: configureLayout())
+    private lazy var searchController = UISearchController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureSubviews()
+        configureView()
+        configureConstraints()
+    }
+}
+
+// MARK: Configuration
+extension SearchViewController {
+    
+    func configureSubviews() {
+        collectionView.collectionViewLayout = configureLayout()
+        
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
+        
+        initialSnapshot()
+        collectionView.dataSource = dataSource
+    }
+    
+    func configureView() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+        searchController.searchBar.delegate = self
+        view.backgroundColor = .systemBackground
+    }
+    
+    func configureConstraints() {
+        [ collectionView ]
+            .forEach{
+                $0.translatesAutoresizingMaskIntoConstraints = false
+                view.addSubview($0)
+            }
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
+    
+    func configureLayout() -> UICollectionViewLayout {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        return UICollectionViewCompositionalLayout.list(using: configuration)
+    }
+}
