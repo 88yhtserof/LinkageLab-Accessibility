b3fdec758d1571fc36f8fd237ef3e20fea9b7830
88yhtserof <88yhtserofg@gmail.com>
2024-09-20 12:13:56 +0900

    [FEAT] Switch: 목록 화면 구성 및 Switch 동작에 따른 UI 변경 구현 #21

---
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/ButtonViewController/SwitchViewController.swift
@@ -0,0 +1,81 @@
+//
+//  SwitchViewController.swift
+//  DefaultComponents-Accessibility
+//
+//  Created by 링키지랩 on 9/20/24.
+//
+
+import UIKit
+
+final class SwitchViewController: UIViewController {
+    
+    var navigationTitle: String? {
+        didSet {
+            navigationItem.title = navigationTitle
+        }
+    }
+    
+    var dataSource: DataSource!
+    var snapshot: Snapshot!
+    var outlines = Outline.sections
+    var isEnabled: Bool = true
+    
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSubViews()
+        configureView()
+        configureConstraints()
+    }
+}
+
+// MARK: Configuration
+private extension SwitchViewController {
+    func configureSubViews() {
+        let controlCellRegistration = UICollectionView.CellRegistration(handler: controlCellRegistrationHandler)
+        let listCellRegistraion = UICollectionView.CellRegistration(handler: listCellRegistrationHandler)
+        
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier -> UICollectionViewCell in
+            let section = Section(rawValue: indexPath.section) ?? .control
+            switch section {
+            case .control:
+                return collectionView.dequeueConfiguredReusableCell(using: controlCellRegistration, for: indexPath, item: itemIdentifier)
+            case .list:
+                return collectionView.dequeueConfiguredReusableCell(using: listCellRegistraion, for: indexPath, item: itemIdentifier)
+            }
+        })
+        
+        updateSnapshot()
+        collectionView.dataSource = dataSource
+    }
+    
+    func configureView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    func configureConstraints() {
+        [ collectionView ]
+            .forEach{
+                $0.translatesAutoresizingMaskIntoConstraints = false
+                view.addSubview($0)
+            }
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
+}
+
+// MARK: CollectionView Layout
+private extension SwitchViewController {
+    func layout() -> UICollectionViewLayout {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        return UICollectionViewCompositionalLayout.list(using: configuration)
+    }
+}
