796b96be45543c9c751866dde1f1a1d113cc437b
88yhtserof <88yhtserofg@gmail.com>
2024-09-23 10:44:48 +0900

    [FEAT] DateAndTime: DatePicker 배치 #25

--- DefaultComponents-Accessibility/DefaultComponents-Accessibility/PickerViewController/DateAndTimeViewController.swift
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/PickerViewController/DateAndTimeViewController.swift
@@ -9,12 +9,9 @@ import UIKit
 
 final class DateAndTimeViewController: UIViewController {
     
-    var textForPicker: String? {
+    var navigationTitle: String? {
         didSet {
-            if labelForPicker.alpha < 1.0 {
-                labelForPicker.alpha = 1.0
-            }
-            labelForPicker.text = textForPicker
+            navigationItem.title = navigationTitle
         }
     }
     
@@ -27,28 +24,10 @@ final class DateAndTimeViewController: UIViewController {
         }
     }
     
-    var textForColorWell: String? {
-        didSet {
-            if labelForColorWell.alpha < 1.0 {
-                labelForColorWell.alpha = 1.0
-            }
-            labelForColorWell.text = textForColorWell
-        }
-    }
-    
-    private lazy var labelForPicker = UILabel()
-    private lazy var pickerView = UIPickerView()
+    private lazy var dateBoxView = ComponentBoxView([labelForDatePicker, datePicker])
     private lazy var labelForDatePicker = UILabel()
     private lazy var datePicker = UIDatePicker()
-    private lazy var labelForColorWell = UILabel()
-    private lazy var colorWell = UIColorWell()
-    
-    var cities = [
-        "서울",
-        "런던",
-        "도쿄",
-        "뉴욕"
-    ]
+    private lazy var stackView = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,12 +41,10 @@ final class DateAndTimeViewController: UIViewController {
 private extension DateAndTimeViewController {
     func configureSubViews() {
         let labelTexts = [
-            "Picker의 결과를 보여줍니다",
-            "DatePicker의 결과를 보여줍니다",
-            "ColorWell의 결과를 보여줍니다"
+            "DatePicker의 결과를 보여줍니다"
         ]
         
-        [ labelForPicker, labelForDatePicker, labelForColorWell ]
+        [ labelForDatePicker ]
             .enumerated()
             .forEach { (offset, view) in
                 view.text = labelTexts[offset]
@@ -75,13 +52,10 @@ private extension DateAndTimeViewController {
                 view.font = .systemFont(ofSize: 20, weight: .medium)
             }
         
-        pickerView.dataSource = self
-        pickerView.delegate = self
-        
         datePicker.datePickerMode = .dateAndTime
         datePicker.addTarget(self, action: #selector(didSelectDate), for: .valueChanged)
         
-        colorWell.addTarget(self, action: #selector(didSelectColor), for: .valueChanged)
+        dateBoxView.title = "날짜 선택 컴포넌트"
     }
     
     func configureView() {
@@ -89,37 +63,21 @@ private extension DateAndTimeViewController {
     }
     
     func configureConstraints() {
-        [ labelForPicker, datePicker, pickerView, labelForDatePicker, labelForColorWell, colorWell ]
+        [ dateBoxView ]
             .forEach{
-                $0.translatesAutoresizingMaskIntoConstraints = false
-                view.addSubview($0)
+                stackView.addArrangedSubview($0)
             }
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
         
         let verticalInset: CGFloat = 50
         let horizontalInset: CGFloat = 10
         let padding: CGFloat = 8
         
         NSLayoutConstraint.activate([
-            labelForPicker.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: verticalInset),
-            labelForPicker.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
-            
-            pickerView.topAnchor.constraint(equalTo: labelForPicker.bottomAnchor, constant: padding),
-            pickerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
-            pickerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -horizontalInset),
-            
-            labelForDatePicker.topAnchor.constraint(equalTo: pickerView.bottomAnchor, constant: verticalInset),
-            labelForDatePicker.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
-            labelForDatePicker.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -horizontalInset),
-            
-            datePicker.topAnchor.constraint(equalTo: labelForDatePicker.bottomAnchor, constant: padding),
-            datePicker.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
-            
-            labelForColorWell.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: verticalInset),
-            labelForColorWell.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
-            labelForColorWell.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -horizontalInset),
-            
-            colorWell.topAnchor.constraint(equalTo: labelForColorWell.bottomAnchor, constant: padding),
-            colorWell.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset)
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: verticalInset),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: horizontalInset),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -horizontalInset)
         ])
     }
 }
