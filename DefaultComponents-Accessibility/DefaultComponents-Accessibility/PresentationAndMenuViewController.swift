61c46f5a1424964287a345f0e5cbb3be3838767e
88yhtserof <88yhtserofg@gmail.com>
2024-09-23 23:41:53 +0900

    [FEAT] Sheet: Menu 구현 #31

--- DefaultComponents-Accessibility/DefaultComponents-Accessibility/PresentationAndMenuViewController.swift
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/PresentationAndMenuViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SheetViewController.swift
+//  PresentationAndMenuViewController.swift
 //  DefaultComponents-Accessibility
 //
 //  Created by 링키지랩 on 9/23/24.
@@ -7,16 +7,18 @@
 
 import UIKit
 
-final class SheetViewController: UIViewController {
+final class PresentationAndMenuViewController: UIViewController {
     
     var navigationTitle: String? {
         didSet {
             navigationItem.title = navigationTitle
         }
     }
-    
-    private var presentButton = UIButton()
-    private var stackView = UIStackView()
+    private lazy var presentBoxView = ComponentBoxView([presentButton])
+    private lazy var presentButton = UIButton()
+    private lazy var menuBoxView = ComponentBoxView([menuButton])
+    private lazy var menuButton = UIButton()
+    private lazy var stackView = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,12 +29,21 @@ final class SheetViewController: UIViewController {
 }
 
 // MARK: Configuration
-extension SheetViewController {
+extension PresentationAndMenuViewController {
     func configureSubviews() {
-        var config = UIButton.Configuration.filled()
-        config.title = "화면 Present"
-        presentButton.configuration = config
+        var configForPresent = UIButton.Configuration.filled()
+        configForPresent.title = "화면 Present"
+        presentButton.configuration = configForPresent
         presentButton.addTarget(self, action: #selector(didTapPresentButton), for: .touchUpInside)
+        
+        let items = [ changeBackgroundAction(), moreMenu() ]
+        let menu = UIMenu(title: "UIMenu", children: items)
+        var configForeMenu = UIButton.Configuration.filled()
+        configForeMenu.title = "Menu 보기"
+        configForeMenu.subtitle = "버튼을 길게 눌러주세요"
+        configForeMenu.titleAlignment = .center
+        menuButton.menu = menu
+        menuButton.configuration = configForeMenu
         
         stackView.axis = .vertical
         stackView.spacing = 10
@@ -46,7 +57,7 @@ extension SheetViewController {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(stackView)
         
-        [ presentButton ]
+        [ presentBoxView, menuBoxView ]
             .forEach{ stackView.addArrangedSubview($0) }
         
         let horizontalInset: CGFloat = 40
