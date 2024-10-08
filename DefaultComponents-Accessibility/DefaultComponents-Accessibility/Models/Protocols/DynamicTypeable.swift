ceab923bdac2a7e871e59fe22a2b80bf60909ebb
88yhtserof <88yhtserofg@gmail.com>
2024-10-08 10:16:55 +0900

    [FEAT] 프로토콜 DynamicTypeable 생성  #49

---
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/Models/Protocols/DynamicTypeable.swift
@@ -0,0 +1,12 @@
+//
+//  DynamicTypeable.swift
+//  DefaultComponents-Accessibility
+//
+//  Created by 링키지랩 on 10/8/24.
+//
+
+import Foundation
+
+protocol DynamicTypeable {
+    func setPreferredFontyStyle()
+}
