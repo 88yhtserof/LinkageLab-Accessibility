ccfdd6b3bcb00e475b2c804d736a6eda541a40e8
88yhtserof <88yhtserofg@gmail.com>
2024-10-10 10:28:27 +0900

    [FEAT] Calendar: 접근성 개선 여부에 따라 View 구분 및 Date 데이터 변경 #56

--- DefaultComponents-Accessibility/DefaultComponents-Accessibility/DateAndTime/DateAndTimeViewController/DateAndTimeViewController.swift
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/DateAndTime/DateAndTimeViewController/DateAndTimeViewController.swift
@@ -39,6 +39,7 @@ final class DateAndTimeViewController: DefaultCollectionViewController {
     private lazy var countdownPicker = UIDatePicker()
     private lazy var calendarBoxView = ComponentBoxView([calendarView])
     lazy var calendarView = UICalendarView()
+    lazy var calendarViewWithAccessibility = UICalendarView()
     
     init() {
         super.init(isAccessible: true)
@@ -51,9 +52,10 @@ final class DateAndTimeViewController: DefaultCollectionViewController {
         ]
         
         let items = [
-            Item(sectionID: 0, tag: .standard, title: "날짜 선택 컴포넌트", description: "버튼을 통해 날짜를 선택할 수 있는  창이 나타나 날짜를 선택할 수 있습니다.", view: dateBoxView),
+            Item(sectionID: 0, tag: .standard, title: "날짜 선택 컴포넌트", description: "버튼을 통해 날짜를 선택할 수 있는 창이 나타나 날짜를 선택할 수 있습니다.", view: dateBoxView),
             Item(sectionID: 0, tag: .standard, title: "시간 선택 컴포넌트",description: "시간과 분을 선택할 수 있습니다.", view: countdownBoxView),
-            Item(sectionID: 1, tag: .standard, title: "달력 컴포넌트", description: "달력을 볼 수 있는 컴포넌트입니다.", view: calendarBoxView)
+            Item(sectionID: 1, tag: .standard, title: "달력 컴포넌트", description: "달력을 볼 수 있는 컴포넌트입니다.", view: calendarBoxView),
+            Item(sectionID: 1, tag: .improve, title: "달력 컴포넌트", description: "달력을 볼 수 있는 컴포넌트로, 공휴일과 오늘을 기호를 통해 확인할 수 있도록 개선했습니다.", view: calendarViewWithAccessibility)
         ]
         super.sections = sections
         super.items = items
@@ -91,7 +93,9 @@ private extension DateAndTimeViewController {
         
         let dateSelection = UICalendarSelectionSingleDate(delegate: self)
         calendarView.selectionBehavior = dateSelection
-        calendarView.delegate = self
+        
+        calendarViewWithAccessibility.selectionBehavior = dateSelection
+        calendarViewWithAccessibility.delegate = self
     }
 }
 
