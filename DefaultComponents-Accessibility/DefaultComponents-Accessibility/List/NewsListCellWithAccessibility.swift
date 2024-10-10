2fa0e575e475770beb74f16efc22a068d300ce43
88yhtserof <88yhtserofg@gmail.com>
2024-10-10 16:17:13 +0900

    [FEAT] News: 접근성 개선 버전 구현 #55

---
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/List/NewsListCellWithAccessibility.swift
@@ -0,0 +1,65 @@
+//
+//  NewsListCellWithAccessibility.swift
+//  DefaultComponents-Accessibility
+//
+//  Created by 링키지랩 on 10/10/24.
+//
+
+import UIKit
+
+final class NewsListCellWithAccessibility: UITableViewCell {
+    
+    static let reuseIdentifier = "newslistcellwithaccessibility-identifier"
+    
+    var title: String? {
+        didSet {
+            titleLabel.text = title
+            accessibilityLabel = title
+        }
+    }
+    
+    var content: String? {
+        didSet {
+            contentLabel.text = content
+        }
+    }
+    
+    private lazy var titleLabel = UILabel()
+    private lazy var contentLabel = UILabel()
+    private lazy var stackView = UIStackView(arrangedSubviews: [titleLabel, contentLabel])
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureSubviews()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension NewsListCellWithAccessibility {
+    func configureSubviews() {
+        titleLabel.text = title
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        titleLabel.numberOfLines = 1
+        
+        contentLabel.text = content
+        contentLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        contentLabel.numberOfLines = 3
+        
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        
+        accessibilityLabel = title
+        accessibilityHint = "상세 내용을 확인하려면 이중탭 하세요"
+        accessibilityTraits = .button
+    }
+    
+    func configureConstraints() {
+        contentView.addPinnedSubview(stackView, inset: UIEdgeInsets(top: 20, left: 10, bottom: -20, right: -10), height: nil)
+    }
+}
---
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/List/NewsListViewControllerWithAccessibility.swift
@@ -0,0 +1,103 @@
+//
+//  NewsListViewControllerWithAccessibility.swift
+//  DefaultComponents-Accessibility
+//
+//  Created by 링키지랩 on 10/10/24.
+//
+
+import UIKit
+
+final class NewsListViewControllerWithAccessibility: DefaultViewController {
+    
+    var pagable: Pagable<News>!
+    var news = Array(News.samples[..<3])
+    var dataSource: DataSource!
+    var snapshot: Snapshot!
+    
+    private lazy var tableView = UITableView()
+    
+    init() {
+        super.init(nibName: .none, bundle: .none)
+        configureSubviews()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension NewsListViewControllerWithAccessibility {
+    func configureSubviews() {
+        tableView.register(NewsListCellWithAccessibility.self, forCellReuseIdentifier: NewsListCellWithAccessibility.reuseIdentifier)
+        
+        dataSource = DataSource(tableView: tableView) { tableView, indexPath, itemIdentifier in
+            
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsListCellWithAccessibility.reuseIdentifier, for: indexPath) as? NewsListCellWithAccessibility else { return UITableViewCell() }
+            cell.selectionStyle = .none
+            cell.title = self.news[indexPath.row].title
+            cell.content = self.news[indexPath.row].content
+            return cell
+        }
+        initializeSnapshot()
+        tableView.dataSource = dataSource
+        tableView.tableFooterView = ButtonSupplementaryView(title: "더보기")
+        
+        let footerView = ButtonSupplementaryView(title: "더보기")
+        footerView.delegate = self
+        tableView.tableFooterView = footerView
+    }
+    
+    func configureConstraints() {
+        view.addPinnedSubview(tableView, height: nil)
+        
+    }
+}
+
+extension NewsListViewControllerWithAccessibility: ButtonSupplementaryViewDelegate {
+    func buttonSupplementaryView(didTapButton button: UIButton) {
+        if pagable.total == News.samples.count {
+            let alert = UIAlertController(title: "마지막 페이지입니다", message: "모든 페이지를 확인하셨습니다.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .default))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        let startIndex = pagable.lastIndex + 1
+        let expectedEndIndex = pagable.lastIndex + pagable.display
+        
+        let endIndex = expectedEndIndex < News.samples.count ? expectedEndIndex : pagable.lastIndex + (News.samples.count - pagable.lastIndex - 1)
+        
+        let items = Array(News.samples[startIndex...endIndex])
+        updateSnapshot(newItems: items, after: pagable.lastIndex)
+        
+        if UIAccessibility.isVoiceOverRunning {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+                UIAccessibility.post(notification: .announcement, argument: "\(endIndex - startIndex + 1)개의 뉴스가 추가되었습니다")
+            }
+        }
+    }
+}
+
+extension NewsListViewControllerWithAccessibility {
+    typealias DataSource = UITableViewDiffableDataSource<Int, News>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, News>
+    
+    func initializeSnapshot() {
+        pagable = Pagable(total: news.count, start: 0, display: 3, lastIndex: news.count - 1)
+        snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(news)
+        dataSource.apply(snapshot)
+    }
+    
+    func updateSnapshot(newItems items: [News], after index: Int) {
+        pagable.total = pagable.total + items.count
+        pagable.start = news.count
+        pagable.lastIndex = pagable.total - 1
+        snapshot.insertItems(items, afterItem: news[index])
+        news.append(contentsOf: items)
+        
+        dataSource.apply(snapshot)
+    }
+}
