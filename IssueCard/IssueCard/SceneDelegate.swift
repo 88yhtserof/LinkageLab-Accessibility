1e9701d8b8744ab291d7e93f4fff33107af7241a
88yhtserof <88yhtserofg@gmail.com>
2024-10-15 10:19:33 +0900

    [CHORE] 프로젝트 생성 및 code-based 세팅 #61

---
+++ IssueCard/IssueCard/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  IssueCard
+//
+//  Created by 링키지랩 on 10/15/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        
+        let rootViewController = ViewController()
+        self.window?.rootViewController = UINavigationController(rootViewController: rootViewController)
+        self.window?.makeKeyAndVisible()
+    }
+}
+
