c50df926386f8baf87254640ea03afcd18bc9222
88yhtserof <88yhtserofg@gmail.com>
2024-10-21 15:42:33 +0900

    [FEAT] IssueCard-SwiftUI 프로젝트 생성 #71

---
+++ IssueCard-SwiftUI/IssueCard-SwiftUI/IssueCard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IssueCard_SwiftUIApp.swift
+//  IssueCard-SwiftUI
+//
+//  Created by 링키지랩 on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct IssueCard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TaxiCallView()
+        }
+    }
+}
---
+++ IssueCard-SwiftUI/IssueCard-SwiftUI/Isuue1/TaxiCallView.swift
@@ -0,0 +1,20 @@
+//
+//  TaxiCallView.swift
+//  IssueCard-SwiftUI
+//
+//  Created by 링키지랩 on 10/21/24.
+//
+
+import SwiftUI
+import MapKit
+struct TaxiCallView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+#Preview {
+    TaxiCallView()
+}
