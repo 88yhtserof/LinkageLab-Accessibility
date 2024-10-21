0740032890af3b5e1dc3b5336423917764c106ce
88yhtserof <88yhtserofg@gmail.com>
2024-10-21 15:43:46 +0900

    [FEAT] Issue1: UI 구현 완료 #71

--- IssueCard-SwiftUI/IssueCard-SwiftUI/Isuue1/TaxiCallView.swift
+++ IssueCard-SwiftUI/IssueCard-SwiftUI/Isuue1/TaxiCallView.swift
@@ -9,9 +9,45 @@ import SwiftUI
 import MapKit
 struct TaxiCallView: View {
     var body: some View {
-        VStack{
+        MapView()
+        VStack {
+            HStack {
+                Image(systemName: "car")
+//                    .frame(width: 50, height: 50)
+                    .padding(.leading)
+                
+                VStack {
+                    HStack {
+                        Text("일반택시")
+                        
+                        Spacer()
+                        
+                        HStack {
+                            Image(systemName: "info.circle")
+                            Text("예상 22,000")
+                        }
+                    }
+                    HStack {
+                        Text("주변 4인승 택시 호출")
+                        Text("3분")
+                        Spacer()
+                    }
+                }
+                .padding(.leading)
+                .padding(.trailing, 20)
+            }
+            .frame(height: 100)
+            .frame(maxWidth: 380)
+            .background(Color(red: 235 / 255, green: 247 / 255, blue: 255 / 255))
+            .cornerRadius(10)
+            .padding(.horizontal)
+            .padding(.top)
+            
             
         }
+        .frame(maxWidth: .infinity)
+        .background(.white)
+        .cornerRadius(10)
     }
 }
 
