3a1bc2222dc1e7d1fcc8408ce13195912743ad7a
88yhtserof <88yhtserofg@gmail.com>
2024-10-28 11:39:56 +0900

    [FEAT] All: 하단 constraints 조정 #77

--- DefaultComponents-Accessibility/DefaultComponents-Accessibility/Control/SwitchViewController/SwitchViewController.swift
+++ DefaultComponents-Accessibility/DefaultComponents-Accessibility/Control/SwitchViewController/SwitchViewController.swift
@@ -55,20 +55,7 @@ private extension SwitchViewController {
     }
     
     func configureConstraints() {
-        [ collectionView ]
-            .forEach{
-                $0.translatesAutoresizingMaskIntoConstraints = false
-                view.addSubview($0)
-            }
-        
-        let safeArea = view.safeAreaLayoutGuide
-        
-        NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
-        ])
+        view.addPinnedSubview(collectionView, height: nil)
     }
 }
 
