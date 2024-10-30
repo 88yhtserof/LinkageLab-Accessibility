0b13b53dbb796df40498958ddb7398ccd24bebbb
88yhtserof <88yhtserofg@gmail.com>
2024-10-30 10:55:53 +0900

    [FEAT] UIVIew+: addPinnedSubviews 메서드에 anchor 지정 파라미터 추가 #79

--- IssueCard/IssueCard/View+Constraints.swift
+++ IssueCard/IssueCard/View+Constraints.swift
@@ -19,19 +19,35 @@ extension UIView {
     }
     
     /// Adds a view and define constraints to the view.
-    func addPinnedSubview(_ view: UIView, inset: UIEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0), height: CGFloat?) {
+    func addPinnedSubview(_ view: UIView, inset: UIEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0), height: CGFloat?, equalTo anchor: Anchor = Anchor(top: .safeArea, leading: .safeArea, bottom: .safeArea, trailing: .safeArea)) {
         addSubview(view)
         view.translatesAutoresizingMaskIntoConstraints = false
+        let top = anchor.top == .view ? self.topAnchor : safeAreaLayoutGuide.topAnchor
+        let leading = anchor.leading == .view ? self.leadingAnchor : safeAreaLayoutGuide.leadingAnchor
+        let bottom = anchor.bottom == .view ? self.bottomAnchor : safeAreaLayoutGuide.bottomAnchor
+        let trailing = anchor.trailing == .view ? self.trailingAnchor : safeAreaLayoutGuide.trailingAnchor
         
         NSLayoutConstraint.activate([
-            view.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: inset.top),
-            view.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: inset.bottom),
-            view.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: inset.left),
-            view.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: inset.right)
+            view.topAnchor.constraint(equalTo: top, constant: inset.top),
+            view.bottomAnchor.constraint(equalTo: bottom, constant: inset.bottom),
+            view.leadingAnchor.constraint(equalTo: leading, constant: inset.left),
+            view.trailingAnchor.constraint(equalTo: trailing, constant: inset.right)
         ])
 
         if let height = height {
             view.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    struct Anchor {
+        let top: PinnedTo
+        let leading: PinnedTo
+        let bottom: PinnedTo
+        let trailing: PinnedTo
+        
+        enum PinnedTo {
+            case view
+            case safeArea
         }
     }
 }
